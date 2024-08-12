03d53f21c848c75bceb6ecb2d3c484bfd7763fa1
nat-netl <71555737+nat-netl@users.noreply.github.com>
2024-08-12 18:36:01 +0300

    deploy

--- src/redux/store/actionCreators/user.ts
+++ src/redux/store/actionCreators/user.ts
@@ -7,12 +7,25 @@ export const fetchUser = (address: any) => {
   return async (dispatch: Dispatch<any>) => {
     try {
       dispatch({ type: UserActionTypes.FETCH_USER });
-      const response = await axios.get(`${BASE_URL}/api/auth/user-by-address/${address}`);
-      dispatch({ type: UserActionTypes.FETCH_USER_SUCCESS, payload: response.data.user });
-      localStorage.setItem('user', JSON.stringify(response.data.user));
+      const response = await axios.get(
+        `${BASE_URL}/api/auth/user-by-address/${address}`,
+        {
+          headers: {
+            "X-Pinggy-No-Screen": "true",
+          },
+        }
+      );
+      dispatch({
+        type: UserActionTypes.FETCH_USER_SUCCESS,
+        payload: response.data.user,
+      });
+      localStorage.setItem("user", JSON.stringify(response.data.user));
     } catch (error) {
       console.error("Ошибка получения данных:", error);
-      dispatch({ type: UserActionTypes.FETCH_USER_ERROR, payload: `Ошибка получения данных ${error}` });
+      dispatch({
+        type: UserActionTypes.FETCH_USER_ERROR,
+        payload: `Ошибка получения данных ${error}`,
+      });
     }
   };
 };
